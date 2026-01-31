7af27111cc6cc3e6bc2077a2f1c52e30d9ee7f83
Swathi Mudi <swathimudi83@gmail.com>
2026-02-01 01:17:03 +0530

    a

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ connectDB();
 const app = express();
 
 app.use(cors());
+
+app.use(cors({
+  origin: "https://frontend-git-main-mudi-swathi.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
